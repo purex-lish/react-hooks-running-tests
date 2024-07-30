e469b52b784c7991dbc588321ae0e1f5f984d264
Purity Wambua <puritywamb@gmail.com>
2024-07-30 12:26:40 +0300

    react hooks tests

--- src/__tests__/Article.test.js
+++ src/__tests__/Article.test.js
@@ -1,11 +1,18 @@
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 import React from "react";
+function Header(props) {
+  return <h1>hello from the Header!</h1>;
+}
+
+export default Header;
+
 
 import Article from "../components/Article";
 
 test("displays the text 'please pass this test'", () => {
   render(<Article />);
+  screen.debug();
 
   expect(screen.queryByText("please pass this test")).toBeInTheDocument();
 });
--- src/__tests__/Header.test.js
+++ src/__tests__/Header.test.js
@@ -1,8 +1,14 @@
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 import React from "react";
+function Header(props) {
+  return <h1>hello from the Header!</h1>;
+}
 
-import Header from "../components/Header";
+export default Header;
+
+
+
 
 test("displays the text 'hello from the Header!'", () => {
   render(<Header />);
